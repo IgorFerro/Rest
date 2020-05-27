0a5940fe8cd97547859bdf947b7905761039f62e
ifparreira <ifparreira@pagseguro.com>
2020-05-27 15:03:44 -0300

    Firsts Project structures

---
+++ RestAPI/src/main/java/br/go/igor/rest/core/BaseTest.java
@@ -0,0 +1,31 @@
+package br.go.igor.rest.core;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+
+public class BaseTest implements Constantes {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = APP_BASE_URL;
+		RestAssured.port = APP_PORT;
+		RestAssured.basePath = APP_BASE_PATH;
+		
+		RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+		reqBuilder.setAccept(APP_CONTENT_TYPE);
+		RestAssured.requestSpecification = reqBuilder.build();
+		
+		ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+		resBuilder.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+		RestAssured.responseSpecification =resBuilder.build();
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+	}
+
+}
