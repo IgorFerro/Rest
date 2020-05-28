c24cafc35a95ca1f943299c62e786ce74d5943ef
ifparreira <ifparreira@pagseguro.com>
2020-05-28 11:31:44 -0300

    shouldInsertSuccesAccount

--- RestAPI/src/main/java/br/go/igor/rest/tests/BellyTests.java
+++ RestAPI/src/main/java/br/go/igor/rest/tests/BellyTests.java
@@ -1,7 +1,11 @@
 package br.go.igor.rest.tests;
 
-import org.junit.Test;
 import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
 
 import br.go.igor.rest.core.BaseTest;
 
@@ -16,6 +20,35 @@ public class BellyTests extends BaseTest {
 		   .statusCode(401)
 		;
 		
+	}
+	
+	@Test
+	public void shouldInsertSuccesAccount() {
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "wertyui@gmail.com");
+		login.put("senha", "123456");
+		
+		
+	 String token =	given()
+		    .body(login)
+		.when()
+		   .post("/signin")
+		.then()
+		   .statusCode(200)
+		   .extract().path("token");
+	 
+	 
+	     given()
+	       .header("Authorization", "JWT" + token)
+	       .body("{\"nome\": \"conta qualquer igor 666\"}")
+		.when()
+		   .post("/contas")
+		.then()
+		   .statusCode(201)
+		;
+	
 		
 	}
+	
+	
 }
